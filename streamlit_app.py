674ad1c375e3a3dafad1e72f53a550d36e6a4b68
p364980 <102831818+p364980@users.noreply.github.com>
2022-04-01 15:14:34 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,15 @@ import pandas as pd
 
 
 st.sidebar.write("Project name")
+
+mass = st.sidebar.slider('Mass', 0, 100)
+
 df = pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv')
 
 
 st.write("hello world")
 
 st.write(df.head())
-
+df = df[df.mass < (mass * 10000)]
 st.map(df)
 
-mass = st.sidebar.slider('Mass', 0, 100)
