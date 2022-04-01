61921fa1b3878bf53f07f30c47fcfc8d3e208e02
p364980 <102831818+p364980@users.noreply.github.com>
2022-04-01 14:58:36 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,12 @@ import pandas as pd
 
 st.sidebar.write("Project name")
 df = pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv')
+
+
+st.write("hello world")
+
 st.write(df.head())
 
 st.map(df)
 
 st.sidebar.button('Click')
-
-st.write("hello world")
